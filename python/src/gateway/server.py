c21caa8504589e64fb5cd7d1e7d28981a3497a7f
moshu <mxie622@gmail.com>
2023-02-28 13:43:30 +0800

    Update yaml

--- python/src/gateway/server.py
+++ python/src/gateway/server.py
@@ -26,6 +26,8 @@ def login():
 @server.route("/upload", methods = ["POST"])
 def upload():
     access, err = validate.token(request)
+    if err:
+        return err
     access = json.loads(access)
     if access["admin"]:
         if len(request.files) > 1 or len(request.files) < 1:
@@ -40,7 +42,22 @@ def upload():
 
 @server.route("/download", methods=['GET'])
 def download():
-    pass
+    access, err = validate.token(request)
+    if err:
+        return err
+    access = json.loads(access)
+    if access["admin"]:
+        fid_string = request.args.get("fid")
+        if not fid_string:
+            return "fid is required", 400
+        try:
+            out = fs_mp3s.get(ObjectId(fid_string))
+            return send_file(out, download_name=f'{fid_string}.mp3')
+        except Exception as err:
+            print(err)
+            return "internal server error", 500
+    else:
+        return "not authorized", 401
 
 if __name__ == "__main__":
     server.run(host="0.0.0.0", port=9000)
